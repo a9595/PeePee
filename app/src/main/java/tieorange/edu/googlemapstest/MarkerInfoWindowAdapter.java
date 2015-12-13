0ccaa82bc584b824da5491319c61e28406dd84fb
Andrii Kovalchuk <tieorange@gmail.com>
2015-12-13 22:23:28 +0100

    Initial commit

---
+++ app/src/main/java/tieorange/edu/googlemapstest/MarkerInfoWindowAdapter.java
@@ -0,0 +1,13 @@
+package tieorange.edu.googlemapstest;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by tieorange on 11/12/15.
+ */
+
